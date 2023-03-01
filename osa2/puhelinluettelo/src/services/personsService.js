94016b5a73e30a7d13bdbc5a4887fc4521f1aaf8
aaro-p <pennanen.aaro@gmail.com>
2023-03-01 18:00:22 +0200

    Delete commented code

--- osa2/puhelinluettelo/src/services/personsService.js
+++ osa2/puhelinluettelo/src/services/personsService.js
@@ -24,31 +24,6 @@ const deletePerson = (id) => {
     return request.then((response) => response.data);
 };
 
-// {
-//   "persons":[
-//     {
-//       "name": "Arto Hellas",
-//       "number": "040-123456",
-//       "id": 1
-//     },
-//     {
-//       "name": "Ada Lovelace",
-//       "number": "39-44-5323523",
-//       "id": 2
-//     },
-//     {
-//       "name": "Dan Abramov",
-//       "number": "12-43-234345",
-//       "id": 3
-//     },
-//     {
-//       "name": "Mary Poppendieck",
-//       "number": "39-23-6423122",
-//       "id": 4
-//     }
-//   ]
-// }
-
 const personsService = { getPersons, createPerson, updatePerson, deletePerson };
 
 export default personsService;
